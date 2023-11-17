c1ecf6d543b5165546141cd235b40f6cc5f4a376
promac3k <105445463+promac3k@users.noreply.github.com>
2023-11-17 15:04:38 +0000

    .

--- Js/main.js
+++ Js/main.js
@@ -1,3 +1,6 @@
+
+console.log("main.js loaded");
+
 function setCookie(cname, cvalue, exdays) {
   const d = new Date();
   d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
@@ -65,6 +68,52 @@ function mySwitch() {
   }
 }
 
+
 function goBack() {
   window.location.replace(document.referrer);
+}
+
+
+function generateCalendar() {
+  const calendarBody = document.getElementById('calendar-body');
+
+  const months = [
+    { name: 'Setembro', days: 30, firstDay: 5, lastday: 2 },
+    { name: 'Outubro', days: 31, firstDay: 0, lastday: 6 },
+    { name: 'Novembro', days: 30, firstDay: 3, lastday: 4 },
+    { name: 'Dezembro', days: 31, firstDay: 5, lastday: 1 },
+    { name: 'Janeiro', days: 31, firstDay: 1, lastday: 5 },
+    { name: 'Fevereiro', days: 28, firstDay: 4, lastday: 5 },
+    { name: 'Março', days: 31, firstDay: 5, lastday: 1 },
+    { name: 'Abril', days: 30, firstDay: 1, lastday: 6 },
+    { name: 'Maio', days: 31, firstDay: 3, lastday: 3 },
+    { name: 'Junho', days: 30, firstDay: 6, lastday: 1 },
+    { name: 'Julho', days: 31, firstDay: 1, lastday: 5 },
+    { name: 'Agosto', days: 31, firstDay: 4, lastday: 2 },
+    { name: 'Setembro', days: 30, firstDay: 7, lastday: 2 },
+  ];
+
+
+  for (let i = 0; i < months.length; i++) {
+    const monthRow = document.createElement('tr');
+    monthRow.innerHTML = `<td class="month">${months[i].name}</td>`;
+
+    // Get the first day of the month and the total number of days
+    const firstDay = months[i].firstDay;
+    const daysInMonth = months[i].days;
+
+    // Add empty cells for the days before the first day of the month
+    for (let j = 0; j < firstDay; j++) {
+      monthRow.innerHTML += '<td class="empty"></td>';
+    }
+
+    // Add day cells for the entire month with distinctive IDs
+    for (let day = 1; day <= daysInMonth; day++) {
+      const dayId = `${months[i].name}-${day}`;
+      monthRow.innerHTML += `<td id="${dayId}">${day}</td>`;
+    }
+
+
+    calendarBody.appendChild(monthRow);
+  }
 }
