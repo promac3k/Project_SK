a4e224fb497938aec19456f628a27f9f01fbd3df
promac3k <105445463+promac3k@users.noreply.github.com>
2023-11-21 21:34:41 +0000

    .

--- Js/main.js
+++ Js/main.js
@@ -78,19 +78,81 @@ function generateCalendar() {
   const calendarBody = document.getElementById('calendar-body');
 
   const months = [
-    { name: 'Setembro', days: 30, firstDay: 5, lastday: 2 },
-    { name: 'Outubro', days: 31, firstDay: 0, lastday: 6 },
-    { name: 'Novembro', days: 30, firstDay: 3, lastday: 4 },
-    { name: 'Dezembro', days: 31, firstDay: 5, lastday: 1 },
-    { name: 'Janeiro', days: 31, firstDay: 1, lastday: 5 },
-    { name: 'Fevereiro', days: 28, firstDay: 4, lastday: 5 },
-    { name: 'Março', days: 31, firstDay: 5, lastday: 1 },
-    { name: 'Abril', days: 30, firstDay: 1, lastday: 6 },
-    { name: 'Maio', days: 31, firstDay: 3, lastday: 3 },
-    { name: 'Junho', days: 30, firstDay: 6, lastday: 1 },
-    { name: 'Julho', days: 31, firstDay: 1, lastday: 5 },
-    { name: 'Agosto', days: 31, firstDay: 4, lastday: 2 },
-    { name: 'Setembro', days: 30, firstDay: 7, lastday: 2 },
+    { name: 'Setembro', days: 30, firstDay: 5 },
+    { name: 'Outubro', days: 31, firstDay: 0 },
+    { name: 'Novembro', days: 30, firstDay: 3 },
+    { name: 'Dezembro', days: 31, firstDay: 5 },
+    { name: 'Janeiro', days: 31, firstDay: 1 },
+    { name: 'Fevereiro', days: 28, firstDay: 4 },
+    { name: 'Março', days: 31, firstDay: 5 },
+    { name: 'Abril', days: 30, firstDay: 1 },
+    { name: 'Maio', days: 31, firstDay: 3 },
+    { name: 'Junho', days: 30, firstDay: 6 },
+    { name: 'Julho', days: 31, firstDay: 1 },
+    { name: 'Agosto', days: 31, firstDay: 4 },
+    { name: 'Setembro', days: 30, firstDay: 7 },
+  ];
+
+
+  for (let i = 0; i < months.length; i++) {
+    const monthRow = document.createElement('tr');
+    monthRow.innerHTML = `<td class="month">${months[i].name}</td>`;
+
+    // Get the first day of the month and the total number of days
+    const firstDay = months[i].firstDay;
+    const daysInMonth = months[i].days;
+
+    // Add empty cells for the days before the first day of the month
+    for (let j = 0; j < firstDay; j++) {
+      monthRow.innerHTML += '<td class="empty"></td>';
+    }
+
+    /*
+    breve explicacao blocos ciclo 
+        bloco = "E"
+        a = 101 
+        for (let day = 1; day == 120; day++) {
+           const dayId = `${months[i].name}-${bloco+day}`;
+          const td = document.createElement('td');
+          td.id = dayId;
+          td.onclick = function() {
+              alert(this.id);
+          };
+        
+    */
+
+    // Add day cells for the entire month with distinctive IDs
+    for (let day = 1; day <= daysInMonth; day++) {
+      const dayId = `${months[i].name}-${day}`;
+      const td = document.createElement('td');
+      td.id = dayId;
+      td.onclick = function () {
+        alert(this.id);
+      };
+      td.onmousemove = function () {
+        // Adiciona um estilo à borda quando o mouse se move sobre a célula
+        this.style.backgroundColor = "red";
+      };
+      td.onmouseout = function () {
+        // Remove o estilo da borda quando o mouse sai da célula
+        this.style.backgroundColor = "";
+      };
+      td.textContent = day;
+      monthRow.appendChild(td);
+    }
+
+    calendarBody.appendChild(monthRow);
+
+  }
+}
+
+function generateBlocos() {
+  const calendarBody = document.getElementById('calendar-body');
+
+  const months = [
+    { name: 'Piso 1' },
+    { name: 'Piso 2' },
+    { name: 'Piso 3' },
   ];
 
 
@@ -112,13 +174,23 @@ function generateCalendar() {
       const dayId = `${months[i].name}-${day}`;
       const td = document.createElement('td');
       td.id = dayId;
-      td.onclick = function() {
-          alert(this.id);
+      td.onclick = function () {
+        alert(this.id);
       };
+
+      //Here ...
+      td.onmousemove = function () {
+        // Adiciona um estilo à borda quando o mouse se move sobre a célula
+        this.style.backgroundColor = "red";
+      };
+      td.onmouseout = function () {
+        // Remove o estilo da borda quando o mouse sai da célula
+        this.style.backgroundColor = "";
+      };
+
       td.textContent = day;
       monthRow.appendChild(td);
-  }
-
+    }
 
     calendarBody.appendChild(monthRow);
 
