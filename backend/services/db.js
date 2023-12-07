ba5ec9876bd9650e90c3b301f550d14275031012
Sebastiao00 <147596351+Xlcos@users.noreply.github.com>
2023-12-07 20:57:11 +0000

    .

--- backend/services/db.js
+++ backend/services/db.js
@@ -1,33 +1,33 @@
 // Importa o módulo mysql2/promise, que é uma biblioteca para trabalhar com MySQL usando Promises
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2/promise')
 // Importa o módulo de configuração local
-const config = require('../config');
+const config = require('../config')
 
 // Define uma função assíncrona para executar queries SQL
 // Esta função cria uma nova conexão com o banco de dados cada vez que é chamada
 // A conexão é automaticamente fechada após 60 segundos (conforme definido no arquivo de configuração)
-async function query(sql, params) {
+ async function query(sql, params) {
     // Cria uma nova conexão com o banco de dados
-    const connection = await mysql.createConnection(config.db);
+    const connection =  await mysql.createConnection(config.db)
     // Executa a query SQL e retorna os resultados
-    const [results,] = await connection.execute(sql, params);
+    const [results,] =  await connection.execute(sql, params)
 
-    return results;
+    return results
 }
 
 // Define uma função assíncrona para testar a função de query
 // Esta função executa uma query SQL para selecionar todos os registros da tabela 'alunos'
-async function testQuery() {
-    const results = await query("select * from alunos");
+ function testQuery() {
+    const results =  query("select * from alunos")
     // Exibe os resultados da query no console em formato de tabela
-    console.table(results);
+    console.table(results)
 }
 
 // Executa a função de teste
 // Esta linha está comentada, então a função de teste não será executada
-//testQuery()
+testQuery()
 
 // Exporta a função de query para que ela possa ser importada e usada em outros arquivos
-module.exports = {
+/*module.exports = {
     query,
-}
+}*/
