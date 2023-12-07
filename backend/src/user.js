fb28b42a2da1cf3e9e96670434ad817b1993d64a
Sebastiao00 <147596351+Xlcos@users.noreply.github.com>
2023-12-07 10:11:34 +0000

    .

--- backend/src/user.js
+++ backend/src/user.js
@@ -16,25 +16,25 @@ router.use(express.urlencoded({ extended: false }))
 
 router.post("/login", async (req, res) => {
     // Capture the input fields
-    let username = request.body.name;
-    let password = request.body.password;
+    let username = request.body.name
+    let password = request.body.password
     // Ensure the input fields exists and are not empty
     if (username && password) {
         // Execute SQL query that'll select the account from the database based on the specified username and password
         connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function (error, results, fields) {
             // If there is an issue with the query, output the error
-            if (error) throw error;
+            if (error) throw error
             // If the account exists
             if (results.length > 0) {
                 // Authenticate the user
-                request.session.loggedin = true;
-                request.session.username = username;
+                request.session.loggedin = true
+                request.session.username = username
                 // Redirect to home page
-                response.redirect('/pages/index.html');
+                response.redirect('/pages/index.html')
             } else {
-                response.send('Incorrect Username and/or Password!');
+                response.send('Incorrect Username and/or Password!')
             }
-            response.end();
+            response.end()
         });
     } else {
         response.send('Please enter Username and Password!');
